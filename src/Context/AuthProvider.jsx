6fa65e437268167847984695f46c00221537c4d0
mdrubel02 <mdrubel072125@gmail.com>
2023-05-19 23:33:14 +0600

    firebase-setup-envairment-setup

---
+++ src/Context/AuthProvider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const AuthContext=createContext()
+const AuthProvider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AuthProvider;
