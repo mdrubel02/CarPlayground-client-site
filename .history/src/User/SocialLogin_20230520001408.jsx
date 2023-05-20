d144a916efb3d65b482df945f2e5dd4227209542
mdrubel02 <mdrubel072125@gmail.com>
2023-05-20 09:08:53 +0600

    login and register emplemnet

---
+++ .history/src/User/SocialLogin_20230520001408.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Context/AuthProvider';
+
+const SocialLogin = () => {
+    const {signWithGoogle,setLoading} = useContext(AuthContext)
+    let navigate = useNavigate()
+    let location = useLocation()
+    let from = location.state?.from?.pathname || "/";
+
+    const navigation =()=>{
+        navigate(from,{replace:true});
+    }
+
+    const signWithGoogleHandle=()=>{
+        signWithGoogle()
+        .then(result=>{
+            toast.success("Login successfull",{autoClose:1000})
+            
+        })
+        .catch(err=>{
+            setLoading(false)
+            toast.error(err.message,{autoClose:1500})
+        })
+    }
+    return (
+        <div className='relative '>
+                <div className='absolute left-4 top-[20%] text-black' >
+                    <img src={googleImg} alt="" className='w-[30px]' />
+                </div>
+                <button
+                onClick={signWithGoogleHandle}
+                className="py-3 w-full bg-white border border-gray-300 rounded-full shadow-sm flex-grow px-4   font-semibold  outline-none pl-12 bg-none"
+                >SignIn With Google</button>
+            </div>
+    );
+};
+
+export default SocialLogin;
